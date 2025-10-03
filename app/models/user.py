eeddc23d77b5b5cdc0a50dd0e528732f4db428ab
Anshit Chauhan <chauhananshit48@gmail.com>
2025-10-03 21:13:10 +0530

    refactor: modified user table

--- app/models/user.py
+++ app/models/user.py
@@ -2,7 +2,9 @@ from app import db, bcrypt
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)  # email should be unique
     password_hash = db.Column(db.String(128), nullable=False)
 
     def set_password(self, password):
